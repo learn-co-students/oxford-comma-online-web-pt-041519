ed44223aafa4c028c387f88dc47a25de0e829488
zhenggao cai <caizhengao1992@gmail.com>
2019-04-18 07:28:52 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
 
+
+if array.size<2
+first_string=array.join
+
+elsif array.size==2
+first_string=array.join(" and ")
+else 
+last_array=array.last
+array.pop
+first_string=array.join(", ")
+second_string=first_string << ", and #{last_array}"
+end
 end

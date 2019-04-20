fec1cc81c7bf3b6505c67b0bcec98e3b896e6899
Katherine Jablonski <kjablonski08@gmail.com>
2019-04-20 00:14:46 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,9 @@
 def oxford_comma(array)
-  case array.length 
-  when 1 
+  if array.length  == 1 
     array.join 
-  when 2 
+  elsif array.length == 2 
     array.join(" and ")
   else 
-    array[0...-1].join(", ") << (", and #{array[-1]}")
+    array[0...-1].join(", ") + (", and #{array[-1]}")
   end
 end

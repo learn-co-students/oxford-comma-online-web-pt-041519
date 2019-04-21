a8d99f8f5520148e400712673dfa539276a4872d
johncban <johncban@gmail.com>
2019-04-21 19:57:18 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array == ["kiwi"]
+    return array.join
+  elsif array == ["kiwi", "durian"]
+    array[-2] << " and "
+    array.join
+  elsif array.length > 2
+    # Source: https://apidock.com/ruby/String/prepend
+    array[-1].prepend "and "
+    array.join(", ")
+  end
 end
